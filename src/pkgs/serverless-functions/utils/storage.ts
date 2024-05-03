715c1b966c0ae606eb252cec92b85dd38917dfc0
lorenzozane <lorzzn@qq.com>
2024-05-03 17:25:50 +0800

    update: redis url use env

--- src/pkgs/serverless-functions/utils/storage.ts
+++ src/pkgs/serverless-functions/utils/storage.ts
@@ -1,9 +1,10 @@
 import { createStorage } from "unstorage"
 import redisDriver from "unstorage/drivers/redis"
 
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379"
 const storage = createStorage({
   driver: redisDriver({
-    url: "redis://127.0.0.1:6379",
+    url: redisUrl,
   }),
 })
 
